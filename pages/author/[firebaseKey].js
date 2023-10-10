faec0189385d52bf044d29deb0c27ee9cc0881b1
Brandon Schnurbusch <b33blbroxx@gmail.com>
2023-10-10 18:55:27 -0500

    got delete working, also fixed authors page to include author info, as i misread the instructions on that assignment.

--- pages/author/[firebaseKey].js
+++ pages/author/[firebaseKey].js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import { getAuthorDetails } from '../../api/mergedData';
@@ -16,14 +17,33 @@ export default function ViewAuthor() {
     getAuthorDetails(firebaseKey).then(setAuthorDetails);
   };
 
+  // // TODO: make call to API layer to get the data
+  // useEffect(() => {
+  //   viewBookDetails(firebaseKey).then(setBookDetails);
+  // }, [firebaseKey]);
+
   useEffect(() => {
     getAuthDetails();
   }, []);
 
   return (
-    <div>{authorDetails.books?.map((book) => (
-      <BookCard key={book.firebaseKey} bookObj={book} onUpdate={getAuthDetails} />
-    ))};
+    <div className="mt-5 d-flex flex-wrap">
+      <div className="d-flex flex-column">
+        <img src={authorDetails.image} alt={authorDetails.title} style={{ width: '300px' }} />
+      </div>
+      <div className="text-white ms-5 details">
+        <h5>
+          {authorDetails?.first_name} {authorDetails?.last_name}
+        </h5>
+        Author Email: <a href={`mailto:${authorDetails.email}`}>{authorDetails.email}</a>
+        <p>{authorDetails.description || ''}</p>
+      </div>
+      <div id="auth-book-card">
+        {authorDetails.books?.map((book) => (
+          <BookCard key={book.firebaseKey} bookObj={book} onUpdate={getAuthDetails} />
+        ))}
+        ;
+      </div>
     </div>
   );
 }
